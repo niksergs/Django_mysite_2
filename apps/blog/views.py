3ec4d8f26a9644bf5a3da6d815a033303f9f88e0
Nikita Sergeev <nikitasergeev1357@gmail.com>
2024-07-24 12:15:40 +0300

    Added PostListView in blog->views.py

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    """"""
+    model = Post                            # Название нашей модели
+    # По умолчанию ListView ищет шаблон с префиксом имени модели и суффиксом _list.html, если не установлено иное.
+    # Это можно переопределить, установив атрибут template_name
+    template_name = 'blog/post_list.html'
+    # Переопределим имя Queryset по умолчанию object_list, установив атрибут context_object_name = 'posts'.
+    # Это помогает иметь более удобное для работы имя.
+    context_object_name = 'posts'
