39eb85e37369f25b876c13ce4ab902331769419f
Nikita Sergeev <nikitasergeev1357@gmail.com>
2024-08-06 11:37:24 +0300

    Added functional RSS

--- blog_cbv/urls.py
+++ blog_cbv/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from apps.blog.feeds import LatestPostFeed
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.blog.urls')),
     path('', include('apps.accounts.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),   # HTML-редактор
+    path('ckeditor/', include('ckeditor_uploader.urls')),               # HTML-редактор
+    path('feeds/latest/', LatestPostFeed(), name='latest_post_feed'),   # RSS лента
 ]
 
 if settings.DEBUG:
